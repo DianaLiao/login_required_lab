5b0b6c963452917ade35f7d56677b38cc6aae5c0
Diana Liao <dijali@gmail.com>
2021-03-25 17:34:46 -0400

    commit tree 1245b98e157c2edc76f6201d51009ca0c46572de

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/secrets', to: 'secrets#show', as: 'secrets'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/welcome', to:'sessions#welcome', as: 'welcome'
+  post '/logout', to: 'sessions#destroy', as: 'logout'
+
 end
